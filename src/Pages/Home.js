313d22bebad0e385012b9f2c6e585d907924fe27
Ishan-2003 <21BCS022@nith.ac.in>
2024-09-14 03:50:05 +0530

    Quickshell Frontend Assignment
    Quickshell Frontend Assignment

---
+++ src/Pages/Home.js
@@ -0,0 +1,22 @@
+import Button from '../components/Button';
+import React from 'react';
+import MainLayout from '../components/MainLayout';
+import {useState} from 'react';
+
+
+const Home = () => {
+    const [data, setdata] = useState();
+
+    const manageData = (data)=>{
+       setdata(data)
+    };
+
+    return (
+        <div className='home-wrapper'>
+            <Button manageData = {manageData}/>
+            <MainLayout data={data}/>
+        </div>
+    )
+}
+
+export default Home
